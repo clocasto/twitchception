474e2e8c69413d96e778af8fb12405d2d57cf331
Chris LoCasto <locasto1@gmail.com>
2017-01-31 13:17:59 -0600

    renamed frontend script folder

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const chalk = require('chalk');
 
 const PROD = process.env.NODE_ENV === 'production';
 const BUILD_DIR = path.resolve(__dirname, 'public/dist');
-const APP_DIR = path.resolve(__dirname, 'client');
+const APP_DIR = path.resolve(__dirname, 'browser');
 
 console.log(chalk.red('process.env.NODE_ENV'), process.env.NODE_ENV);
 
