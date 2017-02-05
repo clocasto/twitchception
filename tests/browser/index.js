8374715a86e6a56c010e377ca05a0c1463ed6d77
Chris LoCasto <locasto1@gmail.com>
2017-02-05 09:18:09 -0600

    added jsdom to project dev dependencies

--- tests/browser/index.js
+++ tests/browser/index.js
@@ -1,5 +1,5 @@
 /* globals document */
-const jsdom = require('jsdom'); //eslint-disable-line
+const jsdom = require('jsdom'); // eslint-disable-line
 
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = document.defaultView;
